93991ea9a6d3aacb897f28fa7c69eda5ef884385
quarbby <lynnette-ng@outlook.com>
2016-05-12 10:45:28 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Inspired by [SGTaxiHeatMap](https://github.com/JTLX/SGTaxiHeatMap), 
+
+SGTrafficCams displays pins of the 50 traffic cameras around the island
+and their camera image.
+
+Images are updated every minute.
+
+**[View it here](http://penguin.dexots.com)**
---
+++ js/leafletmeth.js
@@ -0,0 +1,62 @@
+var map;
+
+var markers = [];
+var cameras = [];
+
+$(function() {
+    
+    map = L.map('map')
+            .setView([1.3521, 103.8198], 12);
+            
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);    
+      
+    getTrafficImages();
+
+});
+
+// Update every minute
+window.setInterval(function(){
+  getTrafficImages();
+}, 60000);
+
+function getTrafficImages() {
+    console.log("Get Traffic Images");
+    
+    markers = [];
+    cameras = [];
+    
+    var trafficMarker = L.AwesomeMarkers.icon({
+        prefix: 'fa',
+        icon: 'car',
+        markerColor: 'red'
+    });
+    
+    $.ajax({
+        beforeSend: function(request) {
+            request.setRequestHeader('api-key', '<YOUR_API_KEY>');
+        },
+        dataType: "json",
+        url: 'https://api.data.gov.sg/v1/transport/traffic-images',
+        success: function(data) {
+            cameras = data.items[0].cameras;
+            //console.log(cameras);
+            
+            console.log(cameras[0].timestamp);
+            $('#last-update').text(cameras[0].timestamp);
+            
+            cameras.forEach(function(cam) {
+               var marker = L.marker(
+                   [parseFloat(cam.location.latitude), 
+                    parseFloat(cam.location.longitude)],
+                    {icon: trafficMarker})
+                   .addTo(map)
+                   .bindPopup('<p><img src="' + cam.image + '" width="250" height="250""/></p>');
+
+            }); //for each
+            
+        }, // success
+        error: function() {
+            alert("Sorry, error getting data");
+        }
+    });
+}
